7f24ef42dec3d47a44dfb52a1e921c57c1477dc8
Web Developer <username@Lubuntu-VirtualBox.(none)>
2012-11-10 13:42:03 -0700

    store

---
+++ depot/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '40b08f7c5b93488bf108917956f10f2112212987cb2b8b815a692a5a7556d3fd512e93bfef86c975defd94e9e2b9065987e390ac24817e348356bfed2ad63bad'
